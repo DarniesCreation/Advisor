1085f30c9ea5cea708bff7d5ba47ab090c06dadd
DarniesCreation <kentormainacc@gmail.com>
2026-03-27 11:17:21 +0500

    Fix path for deployment

--- run_web.py
+++ run_web.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 import sys
-sys.path.insert(0, '/home/madina/Downloads/Advisor full/files')
-
+import os
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 from advisor.api.main import app
 import uvicorn
-
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
