fa1f0b086f17bd33057688f46d3531af6ebbde89
DarniesCreation <kentormainacc@gmail.com>
2026-03-27 11:38:55 +0500

    Add Dockerfile and fix port

--- run_web.py
+++ run_web.py
@@ -5,4 +5,5 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 from advisor.api.main import app
 import uvicorn
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+	port = int(os.environ.get("PORT", 7860))
+	uvicorn.run(app, host="0.0.0.0", port=port)
